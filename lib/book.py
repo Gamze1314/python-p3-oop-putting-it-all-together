f4f51ff1cd564976f3781fa57a574c737e8cb3c6
gamze <gamzekandara@gmail.com>
2024-05-14 00:11:07 -0400

    Adds shoe.py

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
-class Book:
-    pass
-    
-        
+class Book():
+
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+
+new_book = Book("new" , 50)
+print(new_book.title)
+new_book.turn_page()
